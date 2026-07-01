c7a60837b2f0a5b664b8653360a305e176bc02ca
Sanjay G <gokulesh2615@gmail.com>
2026-07-01 15:11:31 +0530

    feat(config): add default configuration schema

---
+++ configs/config.py
@@ -0,0 +1,23 @@
+import logging
+from logging.config import dictConfig
+
+logging_config = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'default'
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['console']
+    }
+}
+dictConfig(logging_config)
